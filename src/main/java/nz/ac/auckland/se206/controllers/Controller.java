a007dd038f73ca419973991388632d63e4eadd05
Chanul <ceri930@aucklanduni.ac.nz>
2023-09-12 14:10:17 +1200

    Created a Scenemanger class and a Controller class - added variables and methods to scene manager

---
+++ src/main/java/nz/ac/auckland/se206/controllers/Controller.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.scene.control.Label;
+
+public abstract class Controller {
+  private Label timerLabel;
+
+  public void setTimerLabel(Label timerLabel) {
+    this.timerLabel = timerLabel;
+  }
+
+  public Label getTimerLabel() {
+    return timerLabel;
+  }
+}
