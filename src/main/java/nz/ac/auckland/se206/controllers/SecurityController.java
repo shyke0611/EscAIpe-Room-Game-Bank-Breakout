8ec8afb810ec92d72f75c7240e2d9812f9d76f2b
hshi270 <hshi270@aucklanduni.ac.nz>
2023-09-13 22:04:51 +1200

    Implemented code for opening and closing walkie talkie

---
+++ src/main/java/nz/ac/auckland/se206/controllers/SecurityController.java
@@ -0,0 +1,22 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class SecurityController {
+
+  @FXML private VBox walkietalkie;
+
+  @FXML private VBox walkietalkieText;
+
+  private boolean isWalkieTalkieOpened = false;
+
+  // handling mouse events on walkie talkie
+  // open and closes when walkie talkie is clicked
+  @FXML
+  void onWalkieTalkie(MouseEvent event) {
+    isWalkieTalkieOpened = !isWalkieTalkieOpened;
+    walkietalkieText.setVisible(isWalkieTalkieOpened);
+  }
+}
