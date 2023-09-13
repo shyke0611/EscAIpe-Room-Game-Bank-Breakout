12df767e65fd8bf5d5570197bae3495aed94b250
hshi270 <hshi270@aucklanduni.ac.nz>
2023-09-13 16:01:19 +1200

    Added other key components for the difficulty page

--- src/main/java/nz/ac/auckland/se206/controllers/DifficultyController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/DifficultyController.java
@@ -1,5 +1,35 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
 public class DifficultyController {
-    
+
+    @FXML
+    private VBox buttonsContainer;
+    @FXML
+    private Label difficultyLabel;
+    @FXML
+    private ImageView easyAlarmImage;
+    @FXML
+    private VBox easyVbox;
+    @FXML
+    private ImageView hardAlarmImage;
+    @FXML
+    private VBox hardVbox;
+    @FXML
+    private ImageView mediumAlarmImage;
+    @FXML
+    private VBox mediumVbox;
+    @FXML
+    private Button playBtn;
+    @FXML
+    private Label timerLabel;
+    @FXML
+    private Slider timerSlider;
+
 }
