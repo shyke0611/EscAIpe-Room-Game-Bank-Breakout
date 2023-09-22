0623776282407cad3c1e7c0dcd57ad98987ffda0
ishi080 <isaac@aeronet.nz>
2023-09-22 18:16:11 +1200

    Fixed timer starting at 30sec

--- src/main/java/nz/ac/auckland/se206/TimerControl.java
+++ src/main/java/nz/ac/auckland/se206/TimerControl.java
@@ -61,7 +61,7 @@ public class TimerControl {
     // set initial count(seconds) based on minutes
     switch (minutes) {
       case 2:
-        initialCount = 32;
+        initialCount = 120;
         break;
       case 4:
         initialCount = 240;
