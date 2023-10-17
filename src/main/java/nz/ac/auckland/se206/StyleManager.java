3a81ac4f9937e3aa6c8e9fbd427f33f288d0b017
hshi270 <hshi270@aucklanduni.ac.nz>
2023-10-17 17:40:31 +1300

    added fix to the codestyle error

--- src/main/java/nz/ac/auckland/se206/StyleManager.java
+++ src/main/java/nz/ac/auckland/se206/StyleManager.java
@@ -61,17 +61,17 @@ public class StyleManager {
    */
   private static String getColourClassForHoverColour(HoverColour colour) {
     switch (colour) {
-      // for red hover colour
       case RED:
+        // for red hover colour
         return "red";
-      // for green hover colour
       case GREEN:
+        // for green hover colour
         return "green";
-      // for orange hover colour
       case ORANGE:
+        // for orange hover colour
         return "orange";
-      // Handle default or unknown cases as needed
       default:
+        // Handle default or unknown cases as needed
         return "";
     }
   }
