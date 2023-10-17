d461e418d1e2d0c1c4a7a4d9179bf27f45be430e
ishi080 <isaac@aeronet.nz>
2023-10-17 19:52:14 +1300

    Set load screen back to main menu

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -185,7 +185,7 @@ public class App extends Application {
     // DONT DELETE, ensures object starts on find keys which is important for ai to work
     // GameManager.completeObjective();
 
-    Parent root = SceneManager.getUiRoot(Scenes.COMPUTER);
+    Parent root = SceneManager.getUiRoot(Scenes.MAIN_MENU);
 
     scene = new Scene(root, 1000, 700);
     stage.setScene(scene);
